6e76f2724af4f4d8c2764e4dc8adbe830786dd87
kh <chinggu88@gmail.com>
2022-05-11 10:44:11 +0900

    pdf view add

---
+++ lib/views/pdf_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class pdf_view extends StatelessWidget {
+  const pdf_view({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
